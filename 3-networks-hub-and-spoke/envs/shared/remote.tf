6d084354c8accfc6b06b16336f36aa771c08a23d
gabrielmm <gabrielmm@ciandt.com>
2026-09-03 13:40:54 -0300

    fix(foundation-deployer): handle single environment deploy across stages and destroy

--- 3-networks-hub-and-spoke/envs/shared/remote.tf
+++ 3-networks-hub-and-spoke/envs/shared/remote.tf
@@ -29,9 +29,9 @@ locals {
   bootstrap_folder_name        = data.terraform_remote_state.bootstrap.outputs.common_config.bootstrap_folder_name
   common_folder_name           = data.terraform_remote_state.org.outputs.common_folder_name
   network_folder_name          = data.terraform_remote_state.org.outputs.network_folder_name
-  development_folder_name      = data.terraform_remote_state.env_development.outputs.env_folder
-  nonproduction_folder_name    = data.terraform_remote_state.env_nonproduction.outputs.env_folder
-  production_folder_name       = data.terraform_remote_state.env_production.outputs.env_folder
+  development_folder_name      = try(data.terraform_remote_state.env_development.outputs.env_folder, "")
+  nonproduction_folder_name    = try(data.terraform_remote_state.env_nonproduction.outputs.env_folder, "")
+  production_folder_name       = try(data.terraform_remote_state.env_production.outputs.env_folder, "")
   net_hub_project_id           = data.terraform_remote_state.org.outputs.net_hub_project_id
   net_hub_project_number       = data.terraform_remote_state.org.outputs.net_hub_project_number
   organization_service_account = data.terraform_remote_state.bootstrap.outputs.organization_step_terraform_service_account_email
